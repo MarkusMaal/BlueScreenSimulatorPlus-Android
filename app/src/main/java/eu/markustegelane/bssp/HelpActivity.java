e3f469fb7e8a08ededebf2583c0521119d2ec79a
MarkusMaal <markus.maal@gmail.com>
2023-05-18 18:59:45 +0300

    Display first draft of documentation in the help screen using Markdown

--- app/src/main/java/eu/markustegelane/bssp/HelpActivity.java
+++ app/src/main/java/eu/markustegelane/bssp/HelpActivity.java
@@ -13,44 +13,74 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
 import java.util.Random;
+
+import io.noties.markwon.Markwon;
 
 public class HelpActivity extends AppCompatActivity {
 
     Bitmap Buffer;
+    boolean showChars = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int swidth = size.x;
-        int sheight = size.y * 2;
-        Bitmap.Config conf = Bitmap.Config.ARGB_8888;
-        Bitmap bmp = Bitmap.createBitmap(swidth, sheight, conf);
-        Random r = new Random();
-        new CountDownTimer(Long.MAX_VALUE, 100) {
-            @Override
-            public void onTick(long l) {
-                Canvas canvas = new Canvas (bmp);
-                Paint tPaint = new Paint();
-                tPaint.setColor(Color.rgb(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
-                tPaint.setFilterBitmap(false);
-                tPaint.setAntiAlias(true);
-                tPaint.setTextSize(r.nextInt(500) + 150f);
-                tPaint.setStyle(Paint.Style.FILL);
-                int symbol = r.nextInt(9999);
-                String htmlSymbol = String.format("&#%s", symbol);
-                canvas.drawText(String.valueOf(Html.fromHtml(htmlSymbol)), r.nextInt(swidth) - 100, r.nextInt(sheight + 100) - 100, tPaint);
-                ((ImageView)findViewById(R.id.imageView3)).setImageBitmap(bmp);
-            }
+        if (showChars) {
+            Display display = getWindowManager().getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            int swidth = size.x;
+            int sheight = size.y * 2;
+            Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+            Bitmap bmp = Bitmap.createBitmap(swidth, sheight, conf);
+            Random r = new Random();
 
-            @Override
-            public void onFinish() {
+            new CountDownTimer(Long.MAX_VALUE, 100) {
+                @Override
+                public void onTick(long l) {
+                    Canvas canvas = new Canvas(bmp);
+                    Paint tPaint = new Paint();
+                    tPaint.setColor(Color.rgb(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
+                    tPaint.setFilterBitmap(false);
+                    tPaint.setAntiAlias(true);
+                    tPaint.setTextSize(r.nextInt(500) + 150f);
+                    tPaint.setStyle(Paint.Style.FILL);
+                    int symbol = r.nextInt(9999);
+                    String htmlSymbol = String.format("&#%s", symbol);
+                    canvas.drawText(String.valueOf(Html.fromHtml(htmlSymbol)), r.nextInt(swidth) - 100, r.nextInt(sheight + 100) - 100, tPaint);
+                    ((ImageView) findViewById(R.id.imageView3)).setImageBitmap(bmp);
+                }
 
+                @Override
+                public void onFinish() {
+
+                }
+            }.start();
+        } else {
+            final Markwon markwon = Markwon.create(getWindow().getContext());
+            InputStream in_s = getResources().openRawResource(R.raw.help_docs_en);
+            if (Locale.getDefault().getLanguage().equals("et")) {
+                try {
+                    in_s.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                in_s = getResources().openRawResource(R.raw.help_docs_et);
             }
-        }.start();
+            byte[] b = new byte[0];
+            try {
+                b = new byte[in_s.available()];
+                in_s.read(b);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            String markdownText = new String(b);
+            markwon.setMarkdown(findViewById(R.id.textView6), markdownText);
+        }
         getWindow().setTitle(getString(R.string.help));
+
     }
 }
---
+++ app/src/main/res/raw/help_docs_et.md
@@ -0,0 +1,27 @@
+# Sinise ekraani simulaator pluss Androidile
+Tere tulemast sinise ekraani simulaatori Android versiooni! See on lihtne utilliit, mis võimaldab simuleerida erinevaid veaekraane erinevatest Windowsi versioonidest.
+
+## Eelseadistused
+See äpp kasutab eelseadistusi, mis sisaldavad sätteid enda kohta (Kasutaja konfiguratsioonid) ning milliseid opsüsteeme nad peaksid kasutama ja seeläbi millised suvandid on kasutajale nähtavad (OS mall).
+Neid eelseadistusi talletatakse loendis, mida saab salvestada konfiguratsioonifaili.
+
+Vaikeseadistused on järgmised:
+* Windows 11 (Native, ClearType) [Windows 11]
+* Windows 10 (Native, ClearType) [Windows 10]
+* Windows 8/8.1 (Native, ClearType) [Windows 8/8.1]
+* Windows 7 (640x480, ClearType) [Windows 7]
+* Windows Vista (640x480, Standard) [Windows Vista]
+* Windows XP (640x480, Standard) [Windows XP]
+* Windows 2000 Professional/Server Family (640x480, Standard) [Windows 2000]
+* Windows NT 4.0/3.x (Text mode, Standard) [Windows NT 3.x/4.0]
+* Windows CE 3.0 and later (750x400, Standard) [Windows CE]
+* Windows 9x/Millennium Edition (Text mode, Standard) [Windows 9x/Me]
+* Windows 3.1 (Text mode, Standard) [Windows 3.1x]
+* Windows 1.x/2.x (Text mode, Standard) [Windows 1.x/2.x]
+
+Kõiki neid seadistusi saab valida rippmenüüst, mis asub peamenüüs.
+
+Iga eelseadistuse valimisel kuvatakse erinev arv sinise ekraani valikuid.
+Nagu varem mainitud, kõiki neid suvandeid talletatakse eelseadistuses, mis tähendab, et
+muudatused, mis tehakse ühes eelseadistuses, EI liigu edasi teisele suvandile
+(nt kui keelate vesimärgi mingis eelseadistuses, jääb see sisselülitatuks teistes eelseadistustes).
